991c9386965e1d80fa35c242404eb1989e8741f3
DeroMuli <musembimuli1999@gmail.com>
2023-01-26 14:36:44 +0300

    added a producer

--- index.js
+++ index.js
@@ -12,3 +12,19 @@ const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
 });
+
+const producer = kafka.producer()
+
+// Connect to the producer
+await producer.connect()
+
+// Send an event to the demoTopic topic
+await producer.send({
+  topic: 'demoTopic',
+  messages: [
+    { value: 'Hello micro-services world!' },
+  ],
+});
+
+// Disconnect the producer once we're done
+await producer.disconnect();
