35d8d00a19c445490425e7c899a7e4554832c076
DeroMuli <musembimuli1999@gmail.com>
2023-01-26 15:10:55 +0300

    added an async wrapper funtion for the await functions

--- index.js
+++ index.js
@@ -1,30 +1,35 @@
 const express = require('express');
-const { Kafka } = require('kafkajs')
+const { Kafka } = require('kafkajs');
 
-const kafka = new Kafka({
-  clientId: 'kafka-nodejs-starter',
-  brokers: ['kafka1:9092'],
-});
+(async () => {
+  console.log("Initializing kafka...");
+  const kafka = new Kafka({
+    clientId: 'kafka-nodejs-starter',
+    brokers: ['localhost:9092'],
+  });
+  
+  // Initialize the Kafka producer and consumer
+  const producer = kafka.producer();
 
-const app = express();
-
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
-  console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
-});
-
-const producer = kafka.producer()
-
-// Connect to the producer
-await producer.connect()
-
-// Send an event to the demoTopic topic
-await producer.send({
-  topic: 'demoTopic',
-  messages: [
-    { value: 'Hello micro-services world!' },
-  ],
-});
-
-// Disconnect the producer once we're done
-await producer.disconnect();
+  await producer.connect();
+  console.log("Connected to producer.");
+  
+  // Send an event to the demoTopic topic
+  await producer.send({
+    topic: 'demoTopic',
+    messages: [
+      { value: 'This event came from another service.' },
+    ],
+  });
+  console.log("Produced a message.");
+  
+  // Disconnect the producer once we’re done
+  await producer.disconnect();
+  
+  const app = express();
+  
+  const PORT = process.env.PORT || 3001;
+  app.listen(PORT, () => {
+    console.log(`🎉🎉🎉 Application running on port: ${PORT} 🎉🎉🎉`);
+  });
+})();
